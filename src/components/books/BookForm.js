dd1e640e770ce10e7d5eca4639271fe2987e6288
Dimas Arif Setyawan <dimasarifsetyawan@gmail.com>
2021-09-22 16:25:40 +0700

    forms

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Form, Button } from "react-bootstrap";
 
 const BookForm = (props) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
     const handleSubmit = (event) => {
         event.preventDefault();
+    
         const newBook = {
-            id: Math.floor(Math.random() * 100) +1,
-            title: 'Fs Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio',
-            description: 'Mudah Membuat dan Berbisnis Aplikasi Android dengan Android Studio Penulis: Yudha Yudhanto & Ardhi Wijayanto Pengembangan aplikasi digital untuk perangkat mobile (ponsel pintar dan tablet) merupakan salah satu bidang yang prospektif untuk terus dikembangkan.',
-            image: 'https://cdn.gramedia.com/uploads/items/ID_MMBAA2018MTH05MMBAA.jpg'
+          id: Math.floor(Math.random() * 100) + 1,
+          title: title,
+          description: description,
+          image: image
         }
         props.onCreateBook(newBook);
-    }
+        setTitle('')
+        setDescription('')
+        setImage('')
+      }
   return (
     <div>
         <h3>Book Form Component</h3>
         <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicTitle">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter book title" />
+                <Form.Control 
+                type="text"
+                placeholder="Enter book title"
+                name="title"
+                value={title}
+                onChange={e => setTitle(e.target.value)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicDescription">
                 <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Enter book description" />
+                <Form.Control
+                type="text"
+                placeholder="Enter book description"
+                name="description"
+                value={description}
+                onChange={e => setDescription(e.target.value)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicImage">
                 <Form.Label>Image</Form.Label>
-                <Form.Control type="text" placeholder="Enter image url" />
+                <Form.Control
+                type="text" 
+                placeholder="Enter image url"
+                name="image"
+                value={image}
+                onChange={e => setImage(e.target.value)} />
             </Form.Group>
 
-            <Button variant="success" type="submit">
-                Submit
+            <Button
+            variant="success"
+            type="submit"
+            disabled={!title, !description, !image} >
+                Save
             </Button>
       </Form>
     </div>
